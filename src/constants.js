a3b3c5222c88392ef47ba34b8f646020348d70ad
GitKraken <brarjunkunjithaya@gmail.com>
2024-06-01 20:23:51 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,3 @@
-export const scaleFactor = 4;
-
-
 export const dialogueData = {
     pc: `This is my battlestation. I tinker mostly with Machine Learning Frameworks these days.
       I've made a couple of projects using them. I also like Game-Dev and Linux. Anyway regardless of the language, I just like programming.
@@ -12,11 +9,11 @@ export const dialogueData = {
      <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">ThePrimeagen</a>,
     <a href="https://youtube.com/@optimumtech" target="_blank">Optimum</a> and <a href="https://youtube.com/@Danidev" target="_blank">Dani</a>!`,
     bed: `This where I sleep. Great ideas come when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <a href="https://github.com/BRArjun/BRArjun/blob/master/ArjunResume.pdf" target="_blank">Check it out?</a>
+    resume: `This is my desk and on it is my resume. <a href="./ArjunResume.pdf" target="_blank">Check it out?</a>
     Contact me at brarjunkunjithaya@gmail.com if you have any interesting ideas!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
     Kaboom.js by going through the documentation and experimenting  <a href="https://kaboomjs.com/doc/intro" target="_blank">here</a>.`,
     library: `This is my small library of books.I probably read only through some of them. Who else compulsively buys technical books without ever finishing them?`,
     exit: `If you want to exit my portfolio, just use the Ctrl + W key combination -if you are on a PC/Laptop :). Nice meeting you and hope to see you soon :).`,
-  };
+};
