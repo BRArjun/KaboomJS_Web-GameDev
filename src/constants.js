c713727dcedf4c759d65803d9c63b31fd25bbecd
GitKraken <brarjunkunjithaya@gmail.com>
2024-06-01 20:49:48 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,7 @@ export const dialogueData = {
      <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">ThePrimeagen</a>,
     <a href="https://youtube.com/@optimumtech" target="_blank">Optimum</a> and <a href="https://youtube.com/@Danidev" target="_blank">Dani</a>!`,
     bed: `This where I sleep. Great ideas come when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <a href="#" onclick="window.open('./public/ArjunResume.pdf', '_blank');">Check it out?</a>
+    resume: `This is my desk and on it is my resume. <a href="#" onclick="window.open('ArjunResume.pdf');">Check it out?</a>
     Contact me at brarjunkunjithaya@gmail.com if you have any interesting ideas!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
